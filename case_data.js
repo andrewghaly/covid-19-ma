418c60b4be8033f953601b9235f2a98033cc4800
Andrew Ghaly <andrewmghaly@gmail.com>
2020-03-25 23:29:46 -0400

    updated data  march 25th

--- case_data.js
+++ case_data.js
@@ -3,47 +3,47 @@ export default {
     {
       code: "us-ma-001",
       name: "Barnstable County, MA",
-      value: 30
+      value: 51
     },
     {
       code: "us-ma-003",
       name: "Berkshire County, MA",
-      value: 26
+      value: 71
     },
     {
       code: "us-ma-005",
       name: "Bristol County, MA",
-      value: 25
+      value: 67
     },
     {
       code: "us-ma-007",
       name: "Dukes County, MA",
-      value: 1
+      value: 3
     },
     {
       code: "us-ma-009",
       name: "Essex County, MA",
-      value: 73
+      value: 177
     },
     {
       code: "us-ma-011",
       name: "Franklin County, MA",
-      value: 2
+      value: 14
     },
     {
       code: "us-ma-013",
       name: "Hampden County, MA",
-      value: 15
+      value: 45
     },
     {
       code: "us-ma-015",
       name: "Hampshire County, MA",
-      value: 6
+      value: 11
     },
     {
       code: "us-ma-017",
       name: "Middlesex County, MA",
-      value: 232
+      value: 446
     },
     {
       code: "us-ma-019",
@@ -53,22 +53,22 @@ export default {
     {
       code: "us-ma-021",
       name: "Norfolk County, MA",
-      value: 82
+      value: 222
     },
     {
       code: "us-ma-023",
       name: "Plymouth County, MA",
-      value: 32
+      value: 101
     },
     {
       code: "us-ma-025",
       name: "Suffolk County, MA",
-      value: 154
+      value: 342
     },
     {
       code: "us-ma-027",
       name: "Worcester County, MA",
-      value: 42
+      value: 129
     }
   ]
 };
