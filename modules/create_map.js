632325a8ad096d38cebedd5e3ac63aad5f432790
Andrew <andrewmghaly@gmail.com>
2020-03-28 18:33:16 -0400

    refactored and modularized code. fetches data dynamically

---
+++ modules/create_map.js
@@ -0,0 +1,92 @@
+export default function createMap(data) {
+  const countiesMap = Highcharts.geojson(
+      Highcharts.maps["countries/us/us-ma-all"]
+    ),
+    lines = Highcharts.geojson(
+      Highcharts.maps["countries/us/us-ma-all"],
+      "mapline"
+    ),
+    borderLines = Highcharts.grep(lines, function(l) {
+      return l.properties["hc-group"] === "__border_lines__";
+    }),
+    separatorLines = Highcharts.grep(lines, function(l) {
+      return l.properties["hc-group"] === "__separator_lines__";
+    });
+  Highcharts.each(countiesMap, function(mapPoint) {
+    mapPoint.name = mapPoint.name + ", MA";
+  });
+  document.getElementById("container").innerHTML = "Rendering map...";
+  setTimeout(function() {
+    Highcharts.mapChart("container", {
+      title: {
+        text: "COVID-19 Cases in Massachusetts"
+      },
+      legend: {
+        layout: "vertical",
+        align: "left",
+        floating: true,
+        backgroundColor:
+          (Highcharts.defaultOptions &&
+            Highcharts.defaultOptions.legend &&
+            Highcharts.defaultOptions.legend.backgroundColor) ||
+          "rgba(255, 255, 255, 0.85)"
+      },
+      mapNavigation: {
+        enabled: true
+      },
+      colorAxis: {
+        min: 0,
+        tickInterval: 5,
+        stops: [
+          [0, "#F1EEF6"],
+          [0.65, "#900037"],
+          [1, "#500007"]
+        ],
+        labels: {
+          format: "{value} cases"
+        }
+      },
+      plotOptions: {
+        mapline: {
+          showInLegend: false,
+          enableMouseTracking: false
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      series: [
+        {
+          mapData: countiesMap,
+          data: data,
+          joinBy: ["hc-key", "code"],
+          name: "",
+          tooltip: {
+            valueSuffix: " cases "
+          },
+          borderWidth: 0.5,
+          states: {
+            hover: {
+              color: "#a4edba"
+            }
+          },
+          shadow: false
+        },
+        {
+          type: "mapline",
+          name: "State borders",
+          data: borderLines,
+          color: "white",
+          shadow: false
+        },
+        {
+          type: "mapline",
+          name: "Separator",
+          data: separatorLines,
+          color: "gray",
+          shadow: false
+        }
+      ]
+    });
+  }, 0);
+}
---
+++ modules/fetch_data.js
@@ -0,0 +1,65 @@
+import createMap from "./create_map.js";
+import case_data from "./case_data.js";
+
+let pdfToText = function(data) {
+  pdfjsLib.GlobalWorkerOptions.workerSrc =
+    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.4.456/pdf.worker.min.js";
+
+  pdfjsLib.workerSrc =
+    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.4.456/pdf.worker.min.js";
+  pdfjsLib.cMapUrl = "js/vendor/pdfjs/cmaps/";
+  pdfjsLib.cMapPacked = true;
+
+  return pdfjsLib.getDocument(data).promise.then(function(pdf) {
+    var pages = [];
+    for (var i = 0; i < pdf.numPages; i++) {
+      pages.push(i);
+    }
+    return Promise.all(
+      pages.map(function(pageNumber) {
+        return pdf.getPage(pageNumber + 1).then(function(page) {
+          return page.getTextContent().then(function(textContent) {
+            return textContent.items
+              .map(function(item) {
+                return item.str;
+              })
+              .join(" ");
+          });
+        });
+      })
+    ).then(function(pages) {
+      return pages.join("\r\n");
+    });
+  });
+};
+
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+pdfToText(
+  proxyurl +
+    `https://www.mass.gov/doc/covid-19-cases-in-massachusetts-as-of-march-28-2020/download`
+)
+  .then(function(result) {
+    return result
+      .match(/County\s+(.+)\s+Sex/)[1]
+      .replace("Dukes and Nantucket", "Dukes   0   Nantucket")
+      .split(/\s\s\s/)
+      .map(r => (/\d/.test(r) ? parseInt(r.replace(/\s+/, "")) : r));
+  })
+  .then(function(result) {
+    let lastCounty = "";
+    for (let i = 0; i < result.length; i++) {
+      if (i % 2 === 0) {
+        lastCounty =
+          result[i] === "Unknown" ? result[i] : `${result[i]} County, MA`;
+      } else {
+        case_data.data.filter(c => c.name === lastCounty)[0].value = result[i];
+      }
+    }
+    createMap(case_data.data);
+    const totalCases = case_data.data
+      .map(c => c.value)
+      .reduce((a, b) => a + b, 0);
+    document.getElementById(
+      "total-cases"
+    ).innerText = `Confirmed cases reported = ${totalCases}`;
+  });
