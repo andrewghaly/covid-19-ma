80ea39df102d042b6d756f9b9d707cf0be1a5431
Andrew <andrewmghaly@gmail.com>
2020-04-21 21:53:09 -0400

    hardcoded to last date of old style pdf

--- modules/ping_url.js
+++ modules/ping_url.js
@@ -15,10 +15,10 @@ export default function pingUrl(proxyUrl) {
       "December",
     ];
 
-    const day = date.getDate();
+    const day = 19;
     const month = monthNames[date.getMonth()];
     const year = date.getFullYear();
-    const url = `https://www.mass.gov/doc/covid-19-cases-in-massachusetts-as-of-${month.toLowerCase()}-19-${year}/download`;
+    const url = `https://www.mass.gov/doc/covid-19-cases-in-massachusetts-as-of-${month.toLowerCase()}-${day}-${year}/download`;
     return { day, month, year, url };
   }
 
