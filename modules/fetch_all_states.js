83a65619247615c80c425959fcfe89aca478fbae
Andrew <andrewmghaly@gmail.com>
2020-07-12 15:47:23 -0400

    cleaned up states view some more

--- modules/fetch_all_states.js
+++ modules/fetch_all_states.js
@@ -1,79 +1,78 @@
 import createGraph from "./create_graph.js";
 
-const states = [
-  "AL",
-  "AK",
-  "AS",
-  "AZ",
-  "AR",
-  "CA",
-  "CO",
-  "CT",
-  "DE",
-  "DC",
-  "FL",
-  "GA",
-  "GU",
-  "HI",
-  "ID",
-  "IL",
-  "IN",
-  "IA",
-  "KS",
-  "KY",
-  "LA",
-  "ME",
-  "MD",
-  "MA",
-  "MI",
-  "MN",
-  "MS",
-  "MO",
-  "MT",
-  "NE",
-  "NV",
-  "NH",
-  "NJ",
-  "NM",
-  "NY",
-  "NC",
-  "ND",
-  "MP",
-  "OH",
-  "OK",
-  "OR",
-  "PA",
-  "PR",
-  "RI",
-  "SC",
-  "SD",
-  "TN",
-  "TX",
-  "UT",
-  "VT",
-  "VI",
-  "VA",
-  "WA",
-  "WV",
-  "WI",
-  "WY",
-];
-states.map((s) => fetchDataByState(s));
-function fetchDataByState(state) {
+const states = {
+  AL: "Alabama",
+  AK: "Alaska",
+  AZ: "Arizona",
+  AR: "Arkansas",
+  CA: "California",
+  CO: "Colorado",
+  CT: "Connecticut",
+  DE: "Delaware",
+  DC: "District Of Columbia",
+  FL: "Florida",
+  GA: "Georgia",
+  HI: "Hawaii",
+  ID: "Idaho",
+  IL: "Illinois",
+  IN: "Indiana",
+  IA: "Iowa",
+  KS: "Kansas",
+  KY: "Kentucky",
+  LA: "Louisiana",
+  ME: "Maine",
+  MD: "Maryland",
+  MA: "Massachusetts",
+  MI: "Michigan",
+  MN: "Minnesota",
+  MS: "Mississippi",
+  MO: "Missouri",
+  MT: "Montana",
+  NE: "Nebraska",
+  NV: "Nevada",
+  NH: "New Hampshire",
+  NJ: "New Jersey",
+  NM: "New Mexico",
+  NY: "New York",
+  NC: "North Carolina",
+  ND: "North Dakota",
+  OH: "Ohio",
+  OK: "Oklahoma",
+  OR: "Oregon",
+  PA: "Pennsylvania",
+  RI: "Rhode Island",
+  SC: "South Carolina",
+  SD: "South Dakota",
+  TN: "Tennessee",
+  TX: "Texas",
+  UT: "Utah",
+  VT: "Vermont",
+  VA: "Virginia",
+  WA: "Washington",
+  WV: "West Virginia",
+  WI: "Wisconsin",
+  WY: "Wyoming",
+};
+
+for (let [stateCode, stateName] of Object.entries(states)) {
+  fetchDataByState(stateName, stateCode);
+}
+
+function fetchDataByState(stateName, stateCode) {
   $("#all-states").append(
     `<span style="float:left;">
-    <h3>${state}</h3>
-    <div id="daily-cases-${state}" class="state-graph"></div>
+    <h3 class="center">${stateName}</h3>
+    <div id="daily-cases-${stateCode}" class="state-graph"></div>
     </span>`
   );
   axios
     .get(
-      `https://covidtracking.com/api/v1/states/${state.toLowerCase()}/daily.json`
+      `https://covidtracking.com/api/v1/states/${stateCode.toLowerCase()}/daily.json`
     )
     .then((response) => {
       return createGraph(
         response.data.map((d) => d.positiveIncrease).reverse(),
-        `daily-cases-${state}`
+        `daily-cases-${stateCode}`
       );
     });
 }
